33802db1d5de64d77bf404eb89b261277c49c6c1
ngiye <ngiyegeorge9@gmail.com>
2024-11-20 16:56:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+Honda bike1=new Honda(); // instantiation
+bike1.run(); // access of object of class Honda
+        }
+    }
+//It is running safely
